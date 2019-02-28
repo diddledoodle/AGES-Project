99813cf34902d25578a2872b9e194f4ed7dc9e62
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2019-02-28 14:34:54 -0600

    interactive 1 raycasting tutorial

---
+++ Assets/Scripts/DetectLookedatInteractive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// it detects interactive elements the player is looking at.
+/// https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
+/// </summary>
+
+public class DetectLookedatInteractive : MonoBehaviour
+{
+   
+    [Tooltip("Starting point of raycast used to detect interactives.")]
+    [SerializeField]
+    private Transform raycastOrigin;
+
+    [Tooltip("How far from the raycastOrigin will search for the interactive elements.")]
+    [SerializeField]
+    private float maxRange = 5.0f;
+
+
+    private void FixedUpdate()
+    {
+        Debug.DrawRay(raycastOrigin.position,raycastOrigin.forward * maxRange, Color.red);
+        RaycastHit hitInfo;
+        bool objectWasDetected = Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hitInfo, maxRange);
+
+        if (objectWasDetected)
+        {
+            Debug.Log("Player is looking at:" + (hitInfo.collider.gameObject.name) );
+        } 
+    }
+
+}
