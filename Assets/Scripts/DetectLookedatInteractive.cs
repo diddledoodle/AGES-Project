cdeacb7341b03b6f2d20a77817fa7c3f83dc3bb2
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2019-05-05 15:12:34 -0500

    deleted blank lines of code

--- Assets/Scripts/DetectLookedatInteractive.cs
+++ Assets/Scripts/DetectLookedatInteractive.cs
@@ -9,22 +9,17 @@ using UnityEngine;
 /// </summary>
 
 public class DetectLookedatInteractive : MonoBehaviour
-{
-   
+{  
     [Tooltip("Starting point of raycast used to detect interactives.")]
     [SerializeField]
     private Transform raycastOrigin;
-
     [Tooltip("How far from the raycastOrigin will search for the interactive elements.")]
     [SerializeField]
     private float maxRange = 5.0f;
-
     /// <summary>
     /// Event raised when the player looks at a different interactive.
-    /// </summary>
-    
+    /// </summary>  
     public static event Action<IInteractive> LookedAtInteractiveChanged;
-
     public IInteractive LookedAtInteractive
     {
         get { return lookedAtInteractive; }
@@ -36,13 +31,9 @@ public class DetectLookedatInteractive : MonoBehaviour
                 lookedAtInteractive = value;
                 LookedAtInteractiveChanged?.Invoke(lookedAtInteractive);
             }
-        }
-        
+        }     
     }
-
     private IInteractive lookedAtInteractive;
-
-
     private void FixedUpdate()
     {
         LookedAtInteractive = GetLookedAtInteractive();
@@ -51,23 +42,18 @@ public class DetectLookedatInteractive : MonoBehaviour
     /// Raycasts forward from the camera to look for IInteractives.
     /// </summary>
     /// <returns>The first IInteractive detected, or null if none are found.</returns>
-
     private IInteractive GetLookedAtInteractive()
     {
         Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward * maxRange, Color.red);
         RaycastHit hitInfo;
         bool objectWasDetected = Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hitInfo, maxRange);
-
         IInteractive interactive = null;
-
         LookedAtInteractive = interactive;
-
         if (objectWasDetected)
         {
             //Debug.Log("Player is looking at:" + (hitInfo.collider.gameObject.name)");
             interactive = hitInfo.collider.gameObject.GetComponent<IInteractive>();
         }
-
         return interactive;
     }
 }
