92e1b9b62ad821045d15f23ef5790e8b8d526ed6
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2019-05-05 15:16:31 -0500

    deleted white space

--- Assets/Scripts/LookedAtInteractiveDispalyText.cs
+++ Assets/Scripts/LookedAtInteractiveDispalyText.cs
@@ -7,26 +7,21 @@ using UnityEngine.UI;
 /// this UI text displays info about the currently looked at interactive iinteractive.
 /// the text should be hidden if the player is not currently looking at an interactive element.
 /// </summary>
-
 public class LookedAtInteractiveDispalyText : MonoBehaviour
 {
     private IInteractive lookedatInteractive;
     private Text displayText;
-
     private void Awake()
     {
         displayText = GetComponent<Text>();
         UpdateDisplayText();
     }
-
     private void UpdateDisplayText()
     {
         if (lookedatInteractive != null)
             displayText.text = lookedatInteractive.DisplayText;
-
         else
             displayText.text = string.Empty;
-
     }
     /// <summary>
     /// event handler for DetectLookedatInteractive.LookedAtInteractiveChanged
