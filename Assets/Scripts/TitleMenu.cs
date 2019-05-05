fd7d4b0e4b350e76af95307e59745661bf9ceebe
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2019-05-04 23:21:47 -0500

    fixed bugs in main menu added menu animations added menu music

--- Assets/Scripts/TitleMenu.cs
+++ Assets/Scripts/TitleMenu.cs
@@ -6,10 +6,27 @@ public class TitleMenu : MonoBehaviour
     [SerializeField]
     private string gameSceneName;
 
+    [SerializeField]
+    private GameObject creditsMenuPanel;
+
+    [SerializeField]
+    private GameObject titleMenuPanel;
+
     public void LoadGameScene()
     {
         SceneManager.LoadScene(gameSceneName);
     }
+
+    public void ShowCredits()
+    {
+        creditsMenuPanel.SetActive(true);
+    }
+
+    public void ShowTitle()
+    {
+        titleMenuPanel.SetActive(true);
+    }
+
 
     public void ExitGame()
     {
