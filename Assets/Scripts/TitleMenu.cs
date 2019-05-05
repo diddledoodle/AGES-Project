decf5bd8d497aead7816f0a21b91d5731e497c99
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2019-05-05 15:18:01 -0500

    deleted whitespace

--- Assets/Scripts/TitleMenu.cs
+++ Assets/Scripts/TitleMenu.cs
@@ -5,29 +5,22 @@ public class TitleMenu : MonoBehaviour
 {
     [SerializeField]
     private string gameSceneName;
-
     [SerializeField]
     private GameObject creditsMenuPanel;
-
     [SerializeField]
     private GameObject titleMenuPanel;
-
     public void LoadGameScene()
     {
         SceneManager.LoadScene(gameSceneName);
     }
-
     public void ShowCredits()
     {
         creditsMenuPanel.SetActive(true);
     }
-
     public void ShowTitle()
     {
         titleMenuPanel.SetActive(true);
     }
-
-
     public void ExitGame()
     {
         Debug.Log("Quit");
