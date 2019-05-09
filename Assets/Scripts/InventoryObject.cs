86aff58c85e48efa2206db8136f51913e5da862c
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2019-05-09 12:56:36 -0500

    Deleted white space

--- Assets/Scripts/InventoryObject.cs
+++ Assets/Scripts/InventoryObject.cs
@@ -7,12 +7,10 @@ public class InventoryObject : InteractiveObject
     [Tooltip("The name of the object as it will appear in the inventory menu UI.")]
     [SerializeField]
     private string objectName = nameof(InventoryObject);
-
     [Tooltip("The text that will display when the player selects this object in the inventory menu.")]
     [TextArea(3,8)]
     [SerializeField]
     private string description;
-
     [Tooltip("Icon to display for this object in the inventory menu.")]
     [SerializeField]
     private Sprite icon;
@@ -39,7 +37,6 @@ public class InventoryObject : InteractiveObject
     /// Can't use destroy because I need to keep the gameobject in the inventory list.
     /// So we just disable the collider and renderer.
     /// </summary>
-
     public override void InteractWith()
     {
         base.InteractWith();
