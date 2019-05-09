f0e63d39c83862127df0e34d435528ebffadee36
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2019-05-09 12:54:19 -0500

    Deleted white space and unused code

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -8,19 +8,15 @@ public class Door : InteractiveObject
     [Tooltip("Assigning a key here will lock the door. If the player has a key in their inventory, they can open the locked door.")]
     [SerializeField]
     private InventoryObject key;
-
     [Tooltip("If this is checked, the associated key will be removed from the player's inventory when the door is unlocked.")]
     [SerializeField]
     private bool consumesKey;
-
     [Tooltip("The text that displays when the player looks at the door while it's locked.")]
     [SerializeField]
     private string lockedDisplayText = "Locked";
-
     [Tooltip("Play this audio when the player interacts with a locked door.")]
     [SerializeField]
     private AudioClip lockedAudioClip;
-
     [Tooltip("Play this audio when the player opens the door.")]
     [SerializeField]
     private AudioClip openAudioClip;
@@ -30,24 +26,19 @@ public class Door : InteractiveObject
     private Animator animator;
     private bool isOpen = false;
     private int shouldOpen = Animator.StringToHash(nameof(shouldOpen));
-
-    //public override string DisplayText => isLocked ? lockedDisplayText: base.DisplayText;
-
     public override string DisplayText
     {
         get
         {
             string toReturn;
-
+            
             if (isLocked)
                 toReturn = HasKey? $"Use {key.ObjectName}" : lockedDisplayText;
             else
                 toReturn = base.DisplayText;
-
             return toReturn;
         }
     }
-
     /// <summary>
     /// Using a constructor here to intialize displayText in the editor.
     /// </summary>
@@ -61,13 +52,11 @@ public class Door : InteractiveObject
         animator = GetComponent<Animator>();
         InitializeisLocked();
     }
-
     private void InitializeisLocked()
     {
         if (key != null)
             isLocked = true;
     }
-
     public override void InteractWith()
     {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
         if (!isOpen)
@@ -87,7 +76,6 @@ public class Door : InteractiveObject
             base.InteractWith();//this plays a sound effect!
         }
     }
-
     private void UnlockDoor()
     {
         isLocked = false;
