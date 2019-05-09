3b915c021f0383ed176c9502758343fa7d54b161
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2019-05-09 14:46:50 -0500

    added alarm added play sound on trigger script

---
+++ Assets/Scripts/PlaySoundOnTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnTrigger : InteractiveObject
+{
+
+    [Tooltip("This is a seperate audio clip that will play when player interacts with a specific object, such as an alarm.")]
+    [SerializeField]
+    private AudioClip itemClip;
+    [SerializeField]
+    private bool isPlayed;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = itemClip;
+        isPlayed = false;
+    }
+    public override void InteractWith()
+    {
+        base.InteractWith();
+        
+        if (isPlayed)
+        {
+            audioSource.Play();
+        }
+        if (!isPlayed)
+        {
+            audioSource.Stop();
+        }
+    }
+}
